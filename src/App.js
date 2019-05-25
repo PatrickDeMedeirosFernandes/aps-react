46706e6990bfbb882d9755c8d70dfd6de135a342
Patrick Fernandes <781410239>
2019-05-25 16:37:06 -0300

    estruturas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import Login from './Pages/Login'
+import Home from './Pages/Home'
+import store from './redux/store'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Login} />
+          <Route path='/home/:id' component={Home} />
+          <Route path='/home' component={Home} />
+          <Route component={Login} />
+        </Switch>
+      </Router>
+    </Provider>
+  )
 }
 
-export default App;
+export default App
